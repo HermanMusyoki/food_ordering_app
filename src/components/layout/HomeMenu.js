7304f694e5c540474a2ef2b610fc82e17ab929ba
HermanMusyoki <salmantso14@gmail.com>
2024-02-20 22:08:32 +0300

    added HomeMenu.js

---
+++ src/components/layout/HomeMenu.js
@@ -0,0 +1,12 @@
+export default function HomeMenu() {
+    <section>
+        <div className="text-center">
+            <h3 className="uppercase text-grey-500 semi-bold leading-4">
+                Check out
+            </h3>
+            <h2 className="text-primary font-bold text-4xl italic">
+                Menu
+            </h2>
+        </div>
+    </section>
+}
