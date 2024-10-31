a0013e66458747e0c8aef489b82dbf0ec3a1d4eb
HermanMusyoki <salmantso14@gmail.com>
2024-10-31 10:12:02 +0300

    bug fixes

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,6 @@
 import clientPromise from "@/libs/mongoConnect";
 import {UserInfo} from "@/models/UserInfo";
-// import bcrypt from "bcrypt";
+import bcrypt from "bcrypt";
 import * as mongoose from "mongoose";
 import {User} from '@/models/User';
 import NextAuth, {getServerSession} from "next-auth";
